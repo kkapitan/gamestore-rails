4995798ead65261711c5a50f3202eae3c4a39b9b
Krzysztof <krzysztof.kapitan@gmail.com>
2016-03-30 21:23:59 +0200

    Add missing files and fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 require 'api_constraints'
 
-Rails.application.routes.draw do
+Gamestore::Application.routes.draw do
   devise_for :users
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+
+  namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show]
     end
