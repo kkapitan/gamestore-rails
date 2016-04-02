d0f8e87f669f56adf6865ff269dc57e3a24f428e
Krzysztof <krzysztof.kapitan@gmail.com>
2016-04-02 16:04:03 +0200

    Add sessions create and delete endpoints with specs

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Gamestore::Application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create]
+
+      delete '/sessions/logout', to: 'sessions#destroy'
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token)}
 
   it { should be_valid }
 
@@ -15,5 +16,24 @@ describe User do
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_authentication_token!" do
+
+    it "generates a unique  token" do
+      Devise.stub(:friendly_token).and_return("anuniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "anuniquetoken123"
+    end
+
+    it "genenerates another token if one was taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "anuniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+
+  end
+
+
 
 end
