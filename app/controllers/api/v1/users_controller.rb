0b5d798a08abd7be6a0b318f9e6401eef26432fd
Krzysztof <krzysztof.kapitan@gmail.com>
2016-04-04 11:12:12 +0200

    Add authorization for the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
-
+  before_action :authenticate_with_token!, only: [:destroy, :update]
+  
   def show
     respond_with User.find(params[:id])
   end
@@ -15,7 +16,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    user = current_user
     if user.update(user_params)
       render json: user, status: 200, location: [:api, user]
     else
@@ -24,8 +25,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
+    current_user.destroy
     head 204
   end
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -59,6 +59,7 @@ describe Api::V1::UsersController do
     context "when successfully updated" do
       before(:each) do
         @user = FactoryGirl.create :user
+        api_authorization_header(@user.auth_token)
         patch :update, {id: @user.id, user: {email: "newmail@example.com" }}
       end
 
@@ -74,6 +75,7 @@ describe Api::V1::UsersController do
     context "when is not updated" do
       before(:each) do
         @user = FactoryGirl.create :user
+        api_authorization_header(@user.auth_token)
         patch :update, {id: @user.id, user: {email: "bademail" }}
       end
 
@@ -95,6 +97,7 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
+      api_authorization_header(@user.auth_token)
       delete :destroy, {id: @user.id }
     end
 
