6d17cc25dd8ca40a2371f17bb161151fe83c43fa
Krzysztof <krzysztof.kapitan@gmail.com>
2016-04-04 15:25:38 +0200

    Add create game endpoint and specs

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,6 +5,19 @@ class Api::V1::GamesController < ApplicationController
     respond_with Game.find(params[:id])
   end
 
+  def create
+    game = Game.new(game_params)
+    print(game_params)
+    if game.save
+      render json: game, status: 201, location: [:api, game]
+    else
+      render json: {errors: game.errors}, status: 422
+    end
+  end
 
+    private
+      def game_params
+        params.require(:game).permit(:title,:description,:price)
+      end
 
 end
--- spec/controllers/api/v1/games_controller_spec.rb
+++ spec/controllers/api/v1/games_controller_spec.rb
@@ -17,6 +17,39 @@ describe Api::V1::GamesController do
 
   end
 
-  
+  describe "POST #create" do
+
+    context "When successfuly created" do
+      before(:each) do
+        @game_attributes = FactoryGirl.attributes_for :game
+        post :create, { game: @game_attributes }
+      end
+
+      it "returns json with information about created object" do
+        game_response = json_response
+        expect(game_response[:title]).to eql @game_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "When creating results in error" do
+      before(:each) do
+        @invalid_game_attributes = { title: "Test game" }
+        post :create, { game: @invalid_game_attributes }
+      end
+
+      it "returns json with information about errors" do
+        game_response = json_response
+        expect(game_response[:errors][:description]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+
+    end
+
+  end
+
+
 
 end
