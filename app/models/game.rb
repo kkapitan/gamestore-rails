9baa1571e95eada4f42c515a10ff981996f78623
Krzysztof <krzysztof.kapitan@gmail.com>
2016-04-06 15:11:39 +0200

    Add game title/price scopes and specs

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@ class Game < ActiveRecord::Base
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
+  scope :filter_by_title, lambda { |keyword| where("lower(title) LIKE ?", "%#{keyword.downcase}%")}
+  scope :above_or_equal_to_price, lambda { |price| where("price >= ?", price)}
+  scope :below_or_equal_to_price, lambda { |price| where("price <= ?", price)}
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -17,4 +17,63 @@ describe Game do
   it { should validate_presence_of :price }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
 
+
+  describe ".filter_by_title" do
+    before(:each) do
+      @game1 = FactoryGirl.create :game, title: "World of Warcraft"
+      @game2 = FactoryGirl.create :game, title: "Witcher 3"
+      @game3 = FactoryGirl.create :game, title: "GTA V"
+      @game4 = FactoryGirl.create :game, title: "Worms"
+    end
+
+    context "when a 'Wo' pattern is sent" do
+
+      it "returns the 2 games matching" do
+        expect(Game.filter_by_title("Wo").count).to eql 2
+      end
+
+      it "returns the games matching" do
+        expect(Game.filter_by_title("Wo")).to match_array([@game1, @game4])
+      end
+
+    end
+
+  end
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @game1 = FactoryGirl.create :game, price: 100
+      @game2 = FactoryGirl.create :game, price: 99
+      @game3 = FactoryGirl.create :game, price: 150
+      @game4 = FactoryGirl.create :game, price: 50
+    end
+
+    it "returns the 2 games matching" do
+      expect(Game.above_or_equal_to_price(100).count).to eql 2
+    end
+
+    it "returns the games matching" do
+      expect(Game.above_or_equal_to_price(100)).to match_array([@game1, @game3])
+    end
+
+  end
+
+  describe ".below_or_equal_to_price" do
+    before(:each) do
+      @game1 = FactoryGirl.create :game, price: 100
+      @game2 = FactoryGirl.create :game, price: 99
+      @game3 = FactoryGirl.create :game, price: 150
+      @game4 = FactoryGirl.create :game, price: 50
+    end
+
+    it "returns the 3 games matching" do
+      expect(Game.below_or_equal_to_price(100).count).to eql 3
+    end
+
+    it "returns the games matching" do
+      expect(Game.below_or_equal_to_price(100)).to match_array([@game2, @game4, @game1])
+    end
+
+  end
+
 end
