b3d877a9db1440b15347965b837fca3bccc42138
Krzysztof <krzysztof.kapitan@gmail.com>
2016-04-10 22:47:07 +0200

    Add details to user model

---
+++ db/migrate/20160410204449_add_details_to_user.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :avatar_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407092642) do
+ActiveRecord::Schema.define(version: 20160410204449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,10 @@ ActiveRecord::Schema.define(version: 20160407092642) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "auth_token",             default: ""
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "nickname"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
